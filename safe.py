e7f1cbecbd5cbc0c920b50067e3a93a258db3701
Benton Johnson <bentonajohnson@gmail.com>
2025-03-06 12:45:51 -0500

    initial commit; base stuff done

---
+++ safe.py
@@ -0,0 +1,111 @@
+from codrone_edu.drone import *
+import time
+
+from scipy.constants import centi
+
+
+def clearmove(dr: Drone):
+    dr.set_pitch(0)
+    dr.set_yaw(0)
+    dr.set_throttle(0)
+
+# init drone
+dr = Drone()
+dr.connect() # open serial conn
+dr.set_trim(-10, 0) # stop drone from flying to the right for no flipping reason :(
+
+def partone(dr: Drone):
+    # Takeoff and hover above the green line thing
+    dr.takeoff()
+    dr.hover(0.5)
+
+    # Go through the first thing and get ready for a figure eight
+    clearmove(dr)
+    dr.set_pitch(50)
+    dr.set_throttle(30)
+    dr.move(1.5)
+
+    # move above the two gates
+    clearmove(dr)
+    dr.set_throttle(50)
+    dr.move(1)
+
+    # past the second gate
+    dr.set_pitch(50)
+    dr.set_throttle(20)
+    dr.move(1.25)
+
+    # move below the second gate
+    clearmove(dr)
+    dr.set_throttle(-80)
+    dr.move(2)
+
+    # back to middle
+    clearmove(dr)
+    dr.set_pitch(-60)
+    dr.set_throttle(40)
+    dr.move(1)
+
+    # up above the first gate
+    clearmove(dr)
+    dr.set_throttle(50)
+    dr.move(1.5)
+
+    # back in front of the first gate
+    clearmove(dr)
+    dr.set_pitch(-95)
+    dr.set_throttle(40)
+    dr.move(0.75)
+
+    # move the drone down to middle front of first gate
+    clearmove(dr)
+    dr.set_throttle(-80)
+    dr.move(3)
+
+    # skip keyhold
+    clearmove(dr)
+    dr.set_pitch(100)
+    dr.set_throttle(50)
+    dr.move(1.5)
+
+def parttwo(dr: Drone):
+    # Finish detecting the color of the second color mat
+    # and fly through the two hoop thingies
+    dr.takeoff() # sleeps
+    clearmove(dr)
+
+    # fly up to meet the first hoop thing
+    dr.set_throttle(40)
+    dr.move(0.75)
+
+    # correct for moving to the side for no fucking reason
+
+    # go through the first hoop thing
+    clearmove(dr)
+    dr.set_roll(50)
+    dr.set_throttle(30)
+    dr.move(2)
+    clearmove(dr)
+
+    # move down a bit to correct for the second hoop
+    clearmove(dr)
+    dr.set_throttle(-60)
+    dr.set_roll(30)
+    dr.move(2)
+
+    # move through the second hoop
+    clearmove(dr)
+    dr.set_pitch(-75)
+    dr.set_throttle(20)
+    dr.move(1.75)
+
+    # correct a bit for movement
+    clearmove(dr)
+    dr.set_throttle(10)
+    dr.set_roll(50)
+    dr.move(0.25)
+
+partone(dr)
+
+dr.land() # land da drone
+dr.close() # close serial conn
