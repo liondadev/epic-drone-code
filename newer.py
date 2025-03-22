0eff4ec9a5da9fd2fe210d7ca38278884747e226
Lion <59421259+liondadev@users.noreply.github.com>
2025-03-22 08:40:10 -0400

    thing

--- newer.py
+++ newer.py
@@ -96,7 +96,7 @@ sleep(2)
 dprint("Going to the top of the two checkpoints.")
 reset()
 dr.get_position_data(0.25)
-send_height_command_meters(2.13)
+send_height_command_meters(1.13)
 sleep(2)
 
 dprint("Going in front of the first checkpoint.")
@@ -108,8 +108,13 @@ sleep(2)
 dprint("Going down to be in front of the first checkpoint.")
 reset()
 dr.get_position_data(0.25)
-send_height_command_meters(-1)
+send_height_command_meters(-1.5)
 sleep(3)
+
+dprint("Aligning with the hole")
+reset()
+dr.get_position_data(0.25)
+dr.move_right(0.5, "ft")
 
 dprint("Going back through the course to get to the second mat.")
 reset()
@@ -118,69 +123,70 @@ dr.reset_move(2)
 move_forward(11-2.13-1, "ft")
 sleep(2)
 
-dprint("Correcting a bit...")
-reset()
-dr.get_position_data(0.25)
-dr.reset_move(2)
-move_right(0.5, "ft")
-
-dprint("Landing")
-dr.land()
-dr.reset_move_values(2)
-
-dprint("== Done!")
-
-################
-## SECOND MAT ##
-################
-
-# repair to fix relativity lol
-dr.disconnect()
-dr.close()
-
-dr = Drone()
-dr.pair()
-
-prefix = "Second Loop"
-dprint("== Performing...")
-
-dr.takeoff()
-dr.hover(1)
-
-dprint("Going to meet first keyhole.")
-reset()
-dr.get_position_data(0.25)
-send_height_command_meters(1)
-sleep(2)
-
-dprint("Going through first keyhole")
-reset()
-dr.get_position_data(0.25)
-dr.move_right(6, "ft")
-sleep(2)
-
-dprint("Threading the drone sized needle!!!!")
-reset()
-dr.get_position_data(0.25)
-send_height_command_meters(-0.25)
-sleep(1)
-
-dprint("Going through second keyhole")
-reset()
-dr.get_position_data(0.25)
-move_backward(4, "ft")
-sleep(2)
-
-dprint("Correcting")
-reset()
-dr.get_position_data(0.25)
-dr.move_left(0.5, "ft")
-sleep(0.5)
-
-dprint("LAND NOW!!")
-dr.land()
-
-dprint("== Done!")
-
-prefix = "Flight Control"
-dprint("Done :)")
+#
+# dprint("Correcting a bit...")
+# reset()
+# dr.get_position_data(0.25)
+# dr.reset_move(2)
+# move_right(0.5, "ft")
+#
+# dprint("Landing")
+# dr.land()
+# dr.reset_move_values(2)
+#
+# dprint("== Done!")
+#
+# ################
+# ## SECOND MAT ##
+# ################
+#
+# # repair to fix relativity lol
+# dr.disconnect()
+# dr.close()
+#
+# dr = Drone()
+# dr.pair()
+#
+# prefix = "Second Loop"
+# dprint("== Performing...")
+#
+# dr.takeoff()
+# dr.hover(1)
+#
+# dprint("Going to meet first keyhole.")
+# reset()
+# dr.get_position_data(0.25)
+# send_height_command_meters(1)
+# sleep(2)
+#
+# dprint("Going through first keyhole")
+# reset()
+# dr.get_position_data(0.25)
+# dr.move_right(6, "ft")
+# sleep(2)
+#
+# dprint("Threading the drone sized needle!!!!")
+# reset()
+# dr.get_position_data(0.25)
+# send_height_command_meters(-0.25)
+# sleep(1)
+#
+# dprint("Going through second keyhole")
+# reset()
+# dr.get_position_data(0.25)
+# move_backward(3.5, "ft")
+# sleep(2)
+#
+# dprint("Correcting")
+# reset()
+# dr.get_position_data(0.25)
+# dr.move_left(0.5, "ft")
+# sleep(0.5)
+#
+# dprint("LAND NOW!!")
+# dr.land()
+#
+# dprint("== Done!")
+#
+# prefix = "Flight Control"
+# dprint("Done :)")
